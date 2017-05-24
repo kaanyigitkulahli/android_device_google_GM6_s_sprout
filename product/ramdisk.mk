9a2b2941440789e63befe330cc9b89a99e7cfa0e
Joe <joe2k01dev@gmail.com>
2017-05-24 16:38:08 +0200

    n383bt: mt6735 => mt6737t

--- product/ramdisk.mk
+++ product/ramdisk.mk
@@ -1,4 +1,4 @@
 # Ramdisk
 PRODUCT_PACKAGES += \
-	$(LOCAL_PATH)/rootdir/etc/fstab.mt6735:root/fstab.mt6735 \
-	$(LOCAL_PATH)/rootdir/etc/init.recovery.mt6735.rc:root/init.recovery.mt6735.rc
+	$(LOCAL_PATH)/rootdir/etc/fstab.mt6737t:root/fstab.mt6737t \
+	$(LOCAL_PATH)/rootdir/etc/init.recovery.mt6737t.rc:root/init.recovery.mt6737t.rc
