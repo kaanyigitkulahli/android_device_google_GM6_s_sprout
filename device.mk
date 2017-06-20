97264abc10f144c932e7fbe61e4748fe02bbb9b4
Rygebin <kaankulahli@gmail.com>
2017-06-20 14:10:17 +0000

    GM6_s_sprout: Fix wrong init code
    Change-Id: Ic16888fbb2d2cd3c5192e76bc210b35295092ac9

--- device.mk
+++ device.mk
@@ -26,9 +26,9 @@ PRODUCT_PACKAGES += \
     fstab.mt6735 \
     init.modem.rc \
     init.mt6735.rc \
-    init.mt6745.usb.rc \
-    init.recovery.mt6735.rc 
-    
+    init.mt6735.usb.rc \
+    init.recovery.mt6735.rc
+
 # Product common configurations
 $(call inherit-product-if-exists, frameworks/native/build/phone-xxhdpi-3072-dalvik-heap.mk)
 $(call inherit-product-if-exists, frameworks/native/build/phone-xxhdpi-3072-hwui-memory.mk)    
